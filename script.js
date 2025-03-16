af1c26dc949be16049c7feae2604774a00c7391c
JOSEURSO <110978290+JOSEURSO@users.noreply.github.com>
2025-03-15 21:04:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -135,6 +135,27 @@ document.addEventListener('contextmenu', function(event) {
 });
 
 
+btnFechar.forEach(button => {
+    button.addEventListener('click', ()=>{
+        modalSite.close()
+        cont = 1;
+    })
+})
+
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key.toLowerCase() === 'u') {
+        event.preventDefault();
+        alert('Visualização do código-fonte desativada!');
+    }
+});
+
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+    alert('Clique direito desativado!');
+});
+
+
+
 
 
 
